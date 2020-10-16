de38a5214a58e4401dac50f096236d1644b08018
CharlotteQuellec <72873567+CharlotteQuellec@users.noreply.github.com>
2020-10-16 16:59:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from math import *
-from enum import Enum
 
 
 class Unit:
@@ -16,33 +15,12 @@ class IndicatorGroup (Enum):
     def __init__(self):
         super().__init__()
 
-    EXPORTS_AND_IMPORTS = 1.1
-    SUPPLY_AND_USE = 1.2
-    PRICES = 1.3
-    FEED_PRICE_RATIOS = 1.4
-    QUANTITIES_FED = 1.5
-    TRANSPORTATION = 1.6
-    ANIMAL_UNIT_INDEXES = 1.7
         
         
-class CommodityGroup (Enum) :
-    
-    def __init__(self):
-        super().__init__()
+class CommodityGroup :
 
-    CORN = 2.1
-    BARLEY = 2.2
-    OATS = 2.3
-    SORGHUM = 2.4
-    BYPRODUCTS_FEEDS = 2.5
-    COARS_GRAINS = 2.6
-    HAY = 2.7
-    FEED_GRAINS = 2.8
-    ANIMAL_PROTEINS_FEEDS = 2.9
-    PROCESSED_FEEDS = 2.01
-    ENERGY_FEEDS = 2.02
-    OTHER = 2.03
-      
+
+    
     
 class Commodity :
     def __init__(self):
@@ -75,3 +53,62 @@ class Measurement:
         self.attribut4 = timeperiodDescr
 
     def Measurement(id: int, year: int, value: float, timeperiodeId: int, timeperiodDescr: str, commodity: Commodity, indicator: Indicator):
+ 
+
+
+
+#Volume
+class Volume ():
+
+    def __init__(self):
+        super().__init__()
+
+    def volume(self,id: int, name: str = 'Volume'):
+        print(self.name + id)
+
+#Surface
+class Surface ():
+
+    def __init__(self, name: str):
+        self.attribut1 = name
+
+    def surface(self,id: int, name: str = "Surface"):
+        print(self.name + id)
+
+#Price
+class Price ():
+
+    def __init__(self):
+        super().__init__()
+
+    def price(self,id: int, name: str = 'Price'):
+        print(self.name + id)
+
+#Count
+class Count ():
+
+    def __init__(self, what: str):
+        self.attribut1 = what
+
+    def count(self,id: int, name: str = "Count", what: str):
+        print(self.name + id)
+
+#Weight
+class Weight ():
+
+    def __init__(self, multiplier: float):
+        self.attribut1 = multiplier
+
+    def weight(self,id: int, name: str = "Weight", multiplier: float):
+        print(self.name + id)
+
+#Ratio
+class Ratio ():
+
+    def __init__(self):
+        super().__init__()
+
+    def ratio(self,id: int, name: str = 'Ratio'):
+        print(self.name + id)
+        
+        
