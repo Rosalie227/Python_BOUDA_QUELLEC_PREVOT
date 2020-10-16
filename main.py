9699fdd791ed636f08b7862cb537a31d26d1e695
Rosalie227 <72817042+Rosalie227@users.noreply.github.com>
2020-10-16 16:53:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import *
+from enum import Enum
 
 
 class Unit:
@@ -15,6 +16,13 @@ class IndicatorGroup (Enum):
     def __init__(self):
         super().__init__()
 
+    EXPORTS_AND_IMPORTS = 1.1
+    SUPPLY_AND_USE = 1.2
+    PRICES = 1.3
+    FEED_PRICE_RATIOS = 1.4
+    QUANTITIES_FED = 1.5
+    TRANSPORTATION = 1.6
+    ANIMAL_UNIT_INDEXES = 1.7
         
         
 class CommodityGroup (Enum) :
@@ -22,7 +30,19 @@ class CommodityGroup (Enum) :
     def __init__(self):
         super().__init__()
 
-    
+    CORN = 2.1
+    BARLEY = 2.2
+    OATS = 2.3
+    SORGHUM = 2.4
+    BYPRODUCTS_FEEDS = 2.5
+    COARS_GRAINS = 2.6
+    HAY = 2.7
+    FEED_GRAINS = 2.8
+    ANIMAL_PROTEINS_FEEDS = 2.9
+    PROCESSED_FEEDS = 2.01
+    ENERGY_FEEDS = 2.02
+    OTHER = 2.03
+      
     
 class Commodity :
     def __init__(self):
